0e81ca3fcf3a2047c307d560039c365a54cebedf
BeatriceCarru <80855260+BeatriceCarru@users.noreply.github.com>
2021-07-23 18:49:17 +0200

    modifica

--- app/src/main/java/math/calculator/Activity_calc.java
+++ app/src/main/java/math/calculator/Activity_calc.java
@@ -1,13 +1,18 @@
 package math.calculator;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.SpannableStringBuilder;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+
 import org.mariuszgromada.math.mxparser.*;
 
 public class Activity_calc extends AppCompatActivity {
@@ -15,17 +20,92 @@ public class Activity_calc extends AppCompatActivity {
     private TextView risultato;
     private EditText schermo;
 
-
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calc);
+        setContentView(R.layout.activity_drawer);
         risultato=findViewById(R.id.risultato);
         schermo=findViewById(R.id.display);
         schermo.setShowSoftInputOnFocus(false); /*Imposta un valore che indica se il metodo di
                                                  input soft verrà reso visibile quando questo
                                                   TextView viene messo a punto. L'impostazione
                                                   predefinita è true */
+        //assegnamento variabile
+        drawerLayout=findViewById(R.id.drawer_layout);
+
+    }
+     public void ClickMenu(View view)
+     {
+         //apro drawer
+        openDrawer(drawerLayout);
+     }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        //apro drawer layout
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    public void ClickLogo(View view)
+    {
+        closeDrawer(drawerLayout);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            //quando il drawer è aperto lo chiudo
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+        }
+    }
+    public void ClickHome(View view)
+    {
+        recreate();
+    }
+
+    public void ClickDashboard(View view){
+        redirectActivity(this, Grafico.class);
+    }
+
+    public void Clickgrafico(View view){
+        redirectActivity(this,Grafico.class);
+    }
+
+
+    public void ClickLogout(View view){
+        logout(this);
+    }
+
+    public static void logout(Activity activity_drawer) {
+        AlertDialog.Builder builder=new AlertDialog.Builder(activity_drawer);
+        builder.setTitle("Logout");
+        builder.setMessage("sei sicuro di voler fare il logout?");
+        builder.setPositiveButton("SI", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //finish activity
+                activity_drawer.finishAffinity();
+                System.exit(0);
+            }
+        });
+        builder.setNegativeButton("N0", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+    protected void onPause(){
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+
+    public static void redirectActivity(Activity activity,Class aClass) {
+        Intent intent=new Intent(activity,aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //start activity
+        activity.startActivity(intent);
 
     }
 
---
+++ app/src/main/java/math/calculator/Grafico.java
@@ -0,0 +1,45 @@
+package math.calculator;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Grafico extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.grafico);
+        drawerLayout=findViewById(R.id.drawer_layout);
+    }
+    public void ClickMenu(View view)
+    {
+        //apro drawer
+        Activity_calc.openDrawer(drawerLayout);
+    }
+    public void ClickLogo(View view){
+        Activity_calc.closeDrawer(drawerLayout);
+    }
+    public void ClickHome(View view){
+        Activity_calc.redirectActivity(this,Activity_calc.class);
+    }
+    public void Dashboard(View view){
+        //crea l'activity
+        recreate();
+    }
+    public void Clickcronologia(View view){
+        Activity_calc.redirectActivity(this,Cronologia.class);
+    }
+    public void ClickLogout(View view){
+        Activity_calc.logout(this);
+    }
+    public void onPause(){
+        super.onPause();
+        Activity_calc.closeDrawer(drawerLayout);
+    }
+
+
+
+}
